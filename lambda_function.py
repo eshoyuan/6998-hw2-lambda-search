43d517db0c40cfa81e13b3655c53ba1fd96b8152
Yixiao Yuan <eshoyuan@gmail.com>
2023-11-12 14:55:38 -0500

    update

--- lambda_function.py
+++ lambda_function.py
@@ -46,6 +46,12 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'body': '',
+        "headers": {
+                "Content-Type": 'application/json',
+                "Access-Control-Allow-Headers": '*',
+                "Access-Control-Allow-Origin": '*',
+                "Access-Control-Allow-Methods": '*',
+        }
     }
 
 
